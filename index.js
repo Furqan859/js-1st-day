f5296084fc7225d50e04ed7fd23b7fc875d8bcae
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2021-11-09 03:42:18 +0500

    make interval time in js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello javascript")
+var countdownElement = document.getElementById('countdown');
+var backImageElement = document.getElementById('bckground-image');
+
+var initialCountdownVal=countdownElement.innerHTML;
+
+
+setInterval(function() {
+    initialCountdownVal = initialCountdownVal > 0 ? initialCountdownVal-1:0;
+
+    CountdownVal=countdownElement.innerHTML
+    countdownElement.innerHTML = initialCountdownVal;
+   var  backImagePath = initialCountdownVal %2 === 0 ? './pictures/sign-up-straw.jpg': './pictures/strawberries.jpg'
+
+   backImageElement.src = backImagePath;
+   console.log('setInterval')
+} , 1000);
+
+
