e7767195f02c0d31bb3a686a15cb1ad7d52d701e
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2021-11-09 05:31:27 +0500

    make css better

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var b = document.getElementById('countdown');
 var i = document.getElementById('background-image');
 
 
- var a = b.innerHTML;
-  
+var a = b.innerHTML;
 
- setInterval(function() {
 
-    
-    a = a > 0 ? a - 1 : 0;
-    
-    b.innerHTML = a;
-    var x = a %2 === 0 ? './pictures/sign-up-straw.jpg': './pictures/strawberries.jpg'
-    i.src=x;
- },1000);
+setInterval(function () {
+
+
+   a = a > 0 ? a - 1 : 0;
+
+   b.innerHTML = a;
+   var x = a % 2 === 0 ? './pictures/1.jpg' : './pictures/2.jpg'
+   i.src = x;
+}, 1000);
 
 
 
@@ -58,5 +58,3 @@ var i = document.getElementById('background-image');
 //    backImageElement.src = backImagePath;
 //    console.log('setInterval')
 // } , 1000);
-
-
